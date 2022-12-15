29d051416cb6c33582e69a32e0010978ea948365
Blue_Gene <khalifaabdallah101@gmail.com>
2022-12-14 16:02:05 -0800

    Create The lobby game mode
    lobby game mode is responsible for checking the numbers of players currently in the lobby map.. after a certain number of players is reached, they travel to the blaster map

---
+++ GameMode/LobbyGameMode.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	//check if a certain number of players have joined before travelling to the map
+	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+
+	if (NumberOfPlayers > 2)
+	{
+		UWorld* World = GetWorld();
+
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/Blastermap?listen"));
+		}
+	}
+}
