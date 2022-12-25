46484f1f1390e55157ca443db764ffd69bb4fae5
Blue_Gene <khalifaabdallah101@gmail.com>
2022-12-25 13:39:05 +0200

    Creating the projectile class

---
+++ Weapon/Projectile.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "Components/BoxComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	//setup collision box, set it as root and configure its collision settings
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	SetRootComponent(CollisionBox);
+	CollisionBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic); //set to wolrd dynamic as a projectile will be moving across the world (not static)
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics); //projectile will be used to generate overlap and hit events
+	CollisionBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore); //ignore all channels, we'll specify our custom channels we want this projectile to overlap/generate hit events with.
+	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility,ECollisionResponse::ECR_Block);  //block the visibility channel.
+	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic,ECollisionResponse::ECR_Block); //block (can hit) world static objects
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
