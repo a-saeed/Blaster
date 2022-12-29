c901b84548e25c061aab82fd00cf50022cb6dd17
Blue_Gene <khalifaabdallah101@gmail.com>
2022-12-29 22:03:27 +0200

    Draw the Crosshairs

--- HUD/BlasterHUD.cpp
+++ HUD/BlasterHUD.cpp
@@ -5,4 +5,62 @@
 
 void ABlasterHUD::DrawHUD()
 {
+	Super::DrawHUD();
+
+	FVector2D ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2D ViewportCenter = (ViewportSize / 2.f, ViewportSize / 2.f);
+
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+
+		if (HUDPackage.CrosshairsTop)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+
+		if (HUDPackage.CrosshairsRight)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+
+		if (HUDPackage.CrosshairsLeft)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+
+		if (HUDPackage.CrosshairsBottom)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBottom, ViewportCenter);
+		}
+	}
+
+}
+
+
+void ABlasterHUD::DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter)
+{
+	float TextureWidth = Texture->GetSizeX();
+	float TextureHeight = Texture->GetSizeY();
+
+	FVector2D TextureDrawPoint = (
+		ViewportCenter - (TextureWidth / 2.f), //Drag half its width and height left and up respectively
+		ViewportCenter - (TextureHeight / 2.f)
+	);
+
+	DrawTexture(Texture,
+		TextureDrawPoint.X,
+		TextureDrawPoint.Y,
+		TextureWidth,
+		TextureHeight,
+		0.f,
+		0.f,
+		1.f,
+		1.f,
+		FLinearColor::White
+	);
 }
