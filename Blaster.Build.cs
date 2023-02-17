bf1b11eba1ec7048f918c346c2b1de5ec3bc0199
Blue_Gene <khalifaabdallah101@gmail.com>
2023-02-17 20:20:32 +0200

    Add Niagara to build

--- Blaster.Build.cs
+++ Blaster.Build.cs
@@ -8,7 +8,7 @@ public class Blaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "SlateCore", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
