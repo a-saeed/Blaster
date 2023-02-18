ee74d097c21735e0d2853a469198614066c7859a
Blue_Gene <khalifaabdallah101@gmail.com>
2023-02-18 15:24:18 +0200

    Overload HitScan weapon Fire() to perform a line trace & apply damage

--- Weapon/HitScanWeapon.cpp
+++ Weapon/HitScanWeapon.cpp
@@ -2,4 +2,66 @@
 
 
 #include "HitScanWeapon.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Sound/SoundCue.h"
 
+void AHitScanWeapon::Fire(const FVector& HitTarget)
+{
+	Super::Fire(HitTarget);
+	/*
+	* -Get the instigator controller from the owner character
+	* -Get muzzle flash socket from the weapon mesh
+	* -Get socket transform which will be used to determine the start point for the line trace
+	* -Extend the end vector by 25% to ensure a target at exactly the end of the line trace 
+	* -Permorm the line trace and coressponding hit particles on all machines..but only apply damage on the server.
+	*/
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());			//Using ACharacter should also work
+	if (!OwnerPawn) return;
+
+	AController* InstigatorController = OwnerPawn->GetController();
+
+	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
+	if (MuzzleFlashSocket && InstigatorController)
+	{
+		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
+		FVector Start = SocketTransform.GetLocation();
+		FVector End = Start + (HitTarget - Start) * 1.25f;
+
+		FHitResult FireHitResult;
+		GetWorld()->LineTraceSingleByChannel(FireHitResult,
+			Start,
+			End,
+			ECollisionChannel::ECC_Visibility
+		);
+
+		if (FireHitResult.bBlockingHit)
+		{
+			ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(FireHitResult.GetActor());
+			if (BlasterCharacter)
+			{
+				if (HasAuthority())
+				{
+					UGameplayStatics::ApplyDamage(
+						BlasterCharacter,
+						Damage,
+						InstigatorController,
+						this,
+						UDamageType::StaticClass());
+				}
+				if (CharacterImpactParticles && CharacterImpactSound)
+				{
+					UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), CharacterImpactParticles, FireHitResult.ImpactPoint, FireHitResult.ImpactNormal.Rotation());
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), CharacterImpactSound, FireHitResult.ImpactPoint);
+				}
+			}
+			if (ImpactParticles && ImpactSound)
+			{
+				UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles, FireHitResult.ImpactPoint, FireHitResult.ImpactNormal.Rotation());
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), ImpactSound, FireHitResult.ImpactPoint);
+			}
+		}
+	}
+}
--- Weapon/HitScanWeapon.h
+++ Weapon/HitScanWeapon.h
@@ -14,4 +14,24 @@ class BLASTER_API AHitScanWeapon : public AWeapon
 {
 	GENERATED_BODY()
 	
+public:
+	
+	virtual void Fire(const FVector& HitTarget) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 10.f;
+
+	UPROPERTY(EditAnywhere)
+	class UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* CharacterImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* CharacterImpactSound;
 };
