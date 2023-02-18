2dc9b5fd669051fedebd240d3a3e14d20b3a673f
Blue_Gene <khalifaabdallah101@gmail.com>
2023-02-18 10:11:49 +0200

    Override PMC functionality to advance next substep
    lec 138

--- Weapon/RocketMovementComponent.cpp
+++ Weapon/RocketMovementComponent.cpp
@@ -3,3 +3,14 @@
 
 #include "RocketMovementComponent.h"
 
+URocketMovementComponent::EHandleBlockingHitResult URocketMovementComponent::HandleBlockingHit(const FHitResult& Hit, float TimeTick, const FVector& MoveDelta, float& SubTickTimeRemaining)
+{
+	Super::HandleBlockingHit(Hit, TimeTick, MoveDelta, SubTickTimeRemaining);
+
+	return EHandleBlockingHitResult::AdvanceNextSubstep;				//Override the default return functionality.. 
+}
+
+void URocketMovementComponent::HandleImpact(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta)
+{
+	//rockts should continue moving; only stop when their CollisionBox(we made) detects a hit.
+}
