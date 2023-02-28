f592311dc8a3654d4e7558446af9fd262ba9f627
Blue_Gene <khalifaabdallah101@gmail.com>
2023-02-28 10:19:00 +0200

    Setup base Pickup class

--- Pickups/Pickup.cpp
+++ Pickups/Pickup.cpp
@@ -2,24 +2,58 @@
 
 
 #include "Pickup.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 
 APickup::APickup()
 {
-
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Overlap Sphere"));
+	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetSphereRadius(150.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);											//no collisions, only overlaps
+	OverlapSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn ,ECollisionResponse::ECR_Overlap); //Overlap only pawns
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pickup mesh"));
+	PickupMesh->SetupAttachment(OverlapSphere);
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);											//Purely visual
 }
 
 void APickup::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority())
+	{
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+	}
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 }
 
 void APickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APickup::Destroyed()
+{
+	Super::Destroyed();
+
+	UGameplayStatics::PlaySoundAtLocation(
+		GetWorld(),
+		PickupSound,
+		GetActorLocation()
+	);
 }
 
