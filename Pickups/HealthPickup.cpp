b476c1a9504f435ba50ee201f186610103968edd
Blue_Gene <khalifaabdallah101@gmail.com>
2023-03-01 07:43:57 +0200

    Create HealthPickup class

---
+++ Pickups/HealthPickup.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+
+AHealthPickup::AHealthPickup()
+{
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
