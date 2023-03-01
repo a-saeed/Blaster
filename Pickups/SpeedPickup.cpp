29accdbf32991c706565e6c4e32f873f770a5336
Blue_Gene <khalifaabdallah101@gmail.com>
2023-03-01 12:00:21 +0200

    Create SpeedPickup class

---
+++ Pickups/SpeedPickup.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpeedPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponents/BuffComponent.h"
+
+void ASpeedPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+		UBuffComponent* Buff = BlasterCharacter->GetBuffComponent();
+		if (Buff)
+		{
+			
+		}
+		Destroy();
+	}
+}
