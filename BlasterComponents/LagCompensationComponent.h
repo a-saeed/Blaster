502be0cec9c4962d363d84e0722b77bb76e870ff
Blue_Gene <khalifaabdallah101@gmail.com>
2023-03-14 06:57:29 +0200

    Setup data structures needed for holding frame packages

--- BlasterComponents/LagCompensationComponent.h
+++ BlasterComponents/LagCompensationComponent.h
@@ -7,6 +7,34 @@
 #include "LagCompensationComponent.generated.h"
 
 
+USTRUCT(BlueprintType)
+struct FBoxInformation
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FVector Location;
+
+	UPROPERTY()
+	FRotator Rotation;
+
+	UPROPERTY()
+	FVector BoxExtent;
+};
+
+USTRUCT(BlueprintType)
+struct FFramePackage
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	float Time;
+
+	UPROPERTY()
+	TMap<FName, FBoxInformation> HitBoxMap;
+};
+
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BLASTER_API ULagCompensationComponent : public UActorComponent
 {
@@ -15,12 +43,21 @@ class BLASTER_API ULagCompensationComponent : public UActorComponent
 public:	
 
 	ULagCompensationComponent();
+	friend class ABlasterCharacter;
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 
 	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY()
+	ABlasterCharacter* BlasterCharacter;
+
+	UPROPERTY()
+	ABlasterPlayerController* BlasterController;
 
 public:	
 };
