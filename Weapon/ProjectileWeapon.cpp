9ba64993f44c7545abe9701388a94e78b8cd1d27
Blue_Gene <khalifaabdallah101@gmail.com>
2023-03-23 11:29:48 +0200

    Remove Authority check from projectile weapon

--- Weapon/ProjectileWeapon.cpp
+++ Weapon/ProjectileWeapon.cpp
@@ -18,30 +18,26 @@ void AProjectileWeapon::Fire(const FVector& HitTarget) //hit target is the impac
 	//has authority checks if this weapon is spawned on the server or not
 	//since its parent weapon class is set to replicate,then so does projectile weapon
 	//after this check we know that projectile weapon has authority on the server only. and the projectile will only spawn on the server. lec 76
-	if (!HasAuthority()) return;
-
-	FActorSpawnParameters SpawnParams;
-
-	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
-	SpawnParams.Owner = GetOwner(); //the owner of this weapon class(i.e. the character).
-	SpawnParams.Instigator = InstigatorPawn; //will be used to grant credit fro elemination.
 
 	//we need a location to spawn our projectile from. use the muzzle socket on the weapon mesh.
 	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
+	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
 
 	if (MuzzleFlashSocket)
 	{
 		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
-		if (ProjectileClass && InstigatorPawn)
-		{
-			FVector ToTarget = HitTarget - SocketTransform.GetLocation();
-			FRotator TargetRotation = ToTarget.Rotation(); //rotation of the projectile so that it faces tha hit target.
 
-			GetWorld()->SpawnActor<AProjectile>(
-				ProjectileClass,
-				SocketTransform.GetLocation(),
-				TargetRotation,
-				SpawnParams);
-		}
+		FVector ToTarget = HitTarget - SocketTransform.GetLocation();
+		FRotator TargetRotation = ToTarget.Rotation(); //rotation of the projectile so that it faces tha hit target.
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = GetOwner(); //the owner of this weapon class(i.e. the character).
+		SpawnParams.Instigator = InstigatorPawn; //will be used to grant credit fro elemination.
+
+		GetWorld()->SpawnActor<AProjectile>(
+			ProjectileClass,
+			SocketTransform.GetLocation(),
+			TargetRotation,
+			SpawnParams);
 	}
 }
