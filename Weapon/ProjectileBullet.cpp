638f41b0f15e5c99eca21b5589f33bcee5fe1798
Blue_Gene <khalifaabdallah101@gmail.com>
2023-03-24 21:46:06 +0200

    Undo ricochet bullet behavior for projectiles

--- Weapon/ProjectileBullet.cpp
+++ Weapon/ProjectileBullet.cpp
@@ -11,7 +11,6 @@ AProjectileBullet::AProjectileBullet()
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
 	ProjectileMovementComponent->bRotationFollowsVelocity = true;
 	ProjectileMovementComponent->SetIsReplicated(true);
-	ProjectileMovementComponent->bShouldBounce = true;
 
 	ProjectileMovementComponent->InitialSpeed = InitialSpeed;
 	ProjectileMovementComponent->MaxSpeed = InitialSpeed;
@@ -19,14 +18,7 @@ AProjectileBullet::AProjectileBullet()
 
 void AProjectileBullet::BeginPlay()
 {
-	AActor::BeginPlay();
-
-	SpawnBulletTracer();
-
-	StartDestroyTimer();
-
-	ProjectileMovementComponent->OnProjectileBounce.AddDynamic(this, &AProjectileBullet::OnBounce);
-
+	Super::BeginPlay();
 	/*
 	//Predict projectile path fo server side rewind
 	FPredictProjectilePathParams PathParams;
@@ -62,55 +54,3 @@ void AProjectileBullet::PostEditChangeProperty(FPropertyChangedEvent& Event)
 	}
 }
 #endif
-
-void AProjectileBullet::OnBounce(const FHitResult& ImpactResult, const FVector& ImpactVelocity)
-{
-	SetImpactSurfaceEffects(ImpactResult.GetActor());
-
-	ACharacter* TargetCharacter = Cast<ACharacter>(ImpactResult.GetActor());
-	if (TargetCharacter)
-	{
-		ProjectileMovementComponent->SetVelocityInLocalSpace(FVector().ZeroVector);
-		if (HasAuthority())
-		{
-			ApplyPointDamage(TargetCharacter);
-		}
-	}
-	
-}
-
-void AProjectileBullet::ApplyPointDamage(AActor* Target)
-{
-	ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
-	if (OwnerCharacter)
-	{
-		if (OwnerCharacter != Target)
-		{
-			AController* OwnerController = OwnerCharacter->Controller;
-			if (OwnerController)
-			{
-				UGameplayStatics::ApplyDamage(Target, Damage, OwnerController, this, UDamageType::StaticClass());
-			}
-		}
-	}
-
-	Destroy();
-}
-
-/*
-void AProjectileBullet::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
-if (OwnerCharacter)
-{
-	AController* OwnerController = OwnerCharacter->Controller;
-	if (OwnerController)
-	{
-		UGameplayStatics::ApplyDamage(OtherActor, Damage, OwnerController, this, UDamageType::StaticClass());
-	}
-}
-
-//the Super version has a call to Destroy. keep super version last.
-Super::OnHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
-}
-*/
--- Weapon/ProjectileBullet.h
+++ Weapon/ProjectileBullet.h
@@ -17,9 +17,6 @@ class BLASTER_API AProjectileBullet : public AProjectile
 public:
 	AProjectileBullet();
 
-	UFUNCTION()																			//must be a ufunction to be bound to a dynamic delegate
-	void OnBounce(const FHitResult& ImpactResult, const FVector& ImpactVelocity);
-
 #if WITH_EDITOR
 	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
 #endif
@@ -29,7 +26,5 @@ protected:
 	virtual void BeginPlay() override;
 
 	//inherting from Projectile, no need for a ufunction macro
-	//virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
-
-	void ApplyPointDamage(AActor* Target);
+	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
 };
