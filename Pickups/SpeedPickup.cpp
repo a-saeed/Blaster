7c9b6f28649171de4ca3e63460ef145c82fe2941
Blue_Gene <khalifaabdallah101@gmail.com>
2023-04-17 10:39:51 +0200

    FIX: Call Super version in Speed pickup

--- Pickups/SpeedPickup.cpp
+++ Pickups/SpeedPickup.cpp
@@ -8,6 +8,8 @@
 void ASpeedPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
 	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
 	if (BlasterCharacter)
 	{
