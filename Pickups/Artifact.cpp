c0333e4588f3a151c785fe6c5be6a26a201f06bf
Blue_Gene <khalifaabdallah101@gmail.com>
2023-04-17 10:29:26 +0200

    Setup overlap events for the artifact

--- Pickups/Artifact.cpp
+++ Pickups/Artifact.cpp
@@ -1,5 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "Artifact.h"
 #include "Components/StaticMeshComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Components/SphereComponent.h"
 AArtifact::AArtifact()
 {
 	SetReplicateMovement(true);
@@ -11,5 +17,45 @@ AArtifact::AArtifact()
 	LightBeam->SetupAttachment(RootComponent);
 	LightBeam->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	LightBeam->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+}
+
+void AArtifact::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME(AArtifact, ArtifactState);
+	DOREPLIFETIME(AArtifact, BlasterOwnerCharacter);
+}
+
+void AArtifact::Tick(float DeltaTime)
+{
+	AActor::Tick(DeltaTime);
+}
+
+void AArtifact::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// only called on the server
+
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	BlasterOwnerCharacter = BlasterOwnerCharacter == nullptr ? Cast<ABlasterCharacter>(OtherActor) : BlasterOwnerCharacter;
+	if (BlasterOwnerCharacter && !BlasterOwnerCharacter->IsElimmed() && GetOwner() == nullptr)
+	{
+		SetOwner(BlasterOwnerCharacter);
+		// disable overlap events
+		GetOverlapSphere()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+		SetArtifactState(EArtifactState::EAS_Equipped);
+	}
+}
+
+void AArtifact::SetArtifactState(EArtifactState NewState)
+{
+	ArtifactState = NewState;
+
+	OnArtifactStateSet();
+}
+
+void AArtifact::OnRep_ArtifactState()
+{
+	OnArtifactStateSet();
 }
 
